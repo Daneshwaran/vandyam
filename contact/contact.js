5e0ac8220734432ac5d3a82c0035c0d66bd45179
katti.shrinidhi <katti.shrinidhi@gmail.com>
2023-02-21 12:12:30 +0530

    vandyam video

---
+++ contact/contact.js
@@ -0,0 +1,29 @@
+
+// var form = document.getElementById('form')
+
+//       form.addEventListener('submit',function(e){
+//         e.preventDefault()
+//         var name = document.getElementById('name').value
+//         var email = document.getElementById('email').value
+//         var mobileNumber = document.getElementById('mobileNumber').value
+//         var message = document.getElementById('message').value
+
+
+
+//         fetch("https://app.vandyam.com/api/contactVandyam",{
+//           method: 'POST',
+//           headers:{
+//             Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZTBkZDVmMmFiYjI1MWI1YzY3YzU2YSIsImlhdCI6MTY3Njg4MDA2MCwiZXhwIjoxNzYzMjgwMDYwfQ.E-QmcM81zcApyfxU8fLcTK0Bw9g9SyxjTR3mvWe8QRw',
+//           },
+
+//           body:{
+//             name:name,
+//             email:email,
+//             mobileNumber:mobileNumber,
+//             message:message
+//           }
+         
+//         })
+//         .then((response) => response.json())
+//         .then((json) => console.log(json))
+//       });
