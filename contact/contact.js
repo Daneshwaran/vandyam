38743253836fd98da89a2f38114190cb49671cac
katti.shrinidhi <katti.shrinidhi@gmail.com>
2023-02-21 15:14:27 +0530

    contact us details post request

--- contact/contact.js
+++ contact/contact.js
@@ -1,29 +1,30 @@
 
-// var form = document.getElementById('form')
+var form = document.getElementById('form')
 
-//       form.addEventListener('submit',function(e){
-//         e.preventDefault()
-//         var name = document.getElementById('name').value
-//         var email = document.getElementById('email').value
-//         var mobileNumber = document.getElementById('mobileNumber').value
-//         var message = document.getElementById('message').value
+      form.addEventListener('submit',function(e){
+        e.preventDefault()
+        var name = document.getElementById('name').value
+        var email = document.getElementById('email').value
+        var mobileNumber = document.getElementById('mobileNumber').value
+        var message = document.getElementById('message').value
 
 
 
-//         fetch("https://app.vandyam.com/api/contactVandyam",{
-//           method: 'POST',
-//           headers:{
-//             Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZTBkZDVmMmFiYjI1MWI1YzY3YzU2YSIsImlhdCI6MTY3Njg4MDA2MCwiZXhwIjoxNzYzMjgwMDYwfQ.E-QmcM81zcApyfxU8fLcTK0Bw9g9SyxjTR3mvWe8QRw',
-//           },
-
-//           body:{
-//             name:name,
-//             email:email,
-//             mobileNumber:mobileNumber,
-//             message:message
-//           }
-         
-//         })
-//         .then((response) => response.json())
-//         .then((json) => console.log(json))
-//       });
+        //fetch('https://jsonplaceholder.typicode.com/posts')
+        fetch("https://app.vandyam.com/api/contactVandyam",{
+          method: 'POST',
+          headers:{
+            "Content-type": "application/json",
+            "Connection":"keep-alive",
+            "Authorization": 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZTBkZDVmMmFiYjI1MWI1YzY3YzU2YSIsImlhdCI6MTY3Njg4MDA2MCwiZXhwIjoxNzYzMjgwMDYwfQ.E-QmcM81zcApyfxU8fLcTK0Bw9g9SyxjTR3mvWe8QRw',
+        },
+          body:JSON.stringify({
+            name:name,
+            email:email,
+            mobileNumber:mobileNumber,
+            message:message
+          })
+        })
+        .then((response) => response.json())
+        .then((json) => console.log(json))
+      });
