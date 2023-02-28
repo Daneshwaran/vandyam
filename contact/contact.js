861f8781511fa3cc4ed3db99e9467d86a13567e6
katti.shrinidhi <katti.shrinidhi@gmail.com>
2023-02-28 17:45:43 +0530

    cotact page validation checks

--- contact/contact.js
+++ contact/contact.js
@@ -1,3 +1,63 @@
+const Toast1 = {
+  init() {
+    this.hideTimeout = null;
+
+    this.el = document.createElement("div");
+    this.el.className = "toast1";
+    document.body.appendChild(this.el);
+  },
+
+  show(message, state) {
+    clearTimeout(this.hideTimeout);
+
+    this.el.textContent = message;
+    this.el.className = "toast1 toast1--visible";
+
+    if (state) {
+      this.el.classList.add(`toast1--${state}`);
+    }
+
+    this.hideTimeout = setTimeout(() => {
+      this.el.classList.remove("toast1--visible");
+    }, 3000);
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => Toast1.init());
+
+function clearErrors(){
+
+  errors = document.getElementsByClassName('formerror');
+  for(let item of errors)
+  {
+      item.innerHTML = "";
+  }
+
+
+}
+
+function validation(){
+  clearErrors();
+  formStatus = true;
+  var mobileNumber = document.getElementById("mobileNumber").value;
+  var email = document.getElementById("email").value;
+  var name = document.getElementById("name").value;
+  if(mobileNumber.length!=10)
+  {
+    document.getElementById("mobileNumberError").innerHTML="Please enter valid Mobile number";
+    formStatus = false;
+  }
+  if(email==''){
+    document.getElementById("emailError").innerHTML="Email cannot be empty";
+    formStatus = false;
+  }
+  if(!name){
+    document.getElementById("nameError").innerHTML="Name cannot be empty";
+    formStatus = false;
+  }
+  return formStatus;
+}
+
 
 var form = document.getElementById('form')
 
@@ -10,7 +70,7 @@ var form = document.getElementById('form')
 
 
 
-        //fetch('https://jsonplaceholder.typicode.com/posts')
+        if( validation()){
         fetch("https://app.vandyam.com/api/contactVandyam",{
           method: 'POST',
           headers:{
@@ -24,7 +84,16 @@ var form = document.getElementById('form')
             mobileNumber:mobileNumber,
             message:message
           })
+        }).then((response) => {
+          if (response.status === 200){
+            Toast1.show('Form Submitted Successfully','success');
+            location.href = `https://www.vandyam.com/`
+            return response.json();
+          }else{
+            Toast1.show('Something Went Wrong Kindly mail to office@vandyam.com','error')
+          }
         })
-        .then((response) => response.json())
-        .then((json) => console.log(json))
+        .then((json) => console.log(json));
+      }
       });
+
